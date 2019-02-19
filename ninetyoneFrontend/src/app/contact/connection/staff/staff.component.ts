2c6c538aa59de269ddb4b5051f779f5308cbe426
Szandi <szalai.sandee@gmail.com>
2019-02-19 02:18:31 +0100

    --

--- ninetyoneFrontend/src/app/contact/connection/staff/staff.component.ts
+++ ninetyoneFrontend/src/app/contact/connection/staff/staff.component.ts
@@ -6,33 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./staff.component.css']
 })
 export class StaffComponent implements OnInit {
-
-  constructor() { }
+  constructor() {   }
 
   navLinks = [
     {
-        label: 'Contact info',
-        link: '../availability',
-        index: 0
+      label: 'Contact info',
+      link: '../availability',
+      index: 0
     },
     {
-        label: 'Apply for job',
-        link: '../jobs',
-        index: 1
+      label: 'Apply for job',
+      link: '../jobs',
+      index: 1
     },
     {
-        label: 'Staff',
-        link: '../staff',
-        index: 2
+      label: 'Staff',
+      link: '../staff',
+      index: 2
     },
     {
-        label: 'Send note about the website',
-        link: '../note',
-        index: 3
+      label: 'Send note about the website',
+      link: '../note',
+      index: 3
     }
- ];
+  ];
 
-  ngOnInit() {
+  async ngOnInit() {
   }
 
 }
---
+++ ninetyoneFrontend/src/app/contact/connection/staff/staff.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class StaffService {
+  /*  private userUrl = 'http://localhost:8080/users';
+    private users  = [];
+
+    constructor(private http: HttpClient) { }
+
+    get_products() {
+        return this.http.get(this.userUrl);
+    }
+
+    getEmployees() {
+        return this.http.get(this.userUrl).subscribe((res: any[]) => {
+            console.log(res);
+            this.users = res;
+        });
+    }*/
+
+}
